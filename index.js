7610d3b973a64f886c93838e06a221ce9808c8cf
Chang Xiong <cnxiong03@gmail.com>
2019-12-09 00:13:11 -0500

    video 11 done

--- index.js
+++ index.js
@@ -7,11 +7,25 @@ console.log('Before');
 //         });
 //     });
 
-getUser(1)
-    .then(user => getRepositories(user.gitHubUsername))
-    .then(repos => getCommits(repos[0]))
-    .then(commits => console.log('Commits', commits))
-    .catch(err => console.log('Error', err.message));
+// getUser(1)
+//     .then(user => getRepositories(user.gitHubUsername))
+//     .then(repos => getCommits(repos[0]))
+//     .then(commits => console.log('Commits', commits))
+//     .catch(err => console.log('Error', err.message));
+
+// Async and Await approach
+async function displayCommits() {
+    try {
+    const user = await getUser(1);
+    const repos =  await getRepositories(user.gitHubUsername);
+    const commits = await getCommits(repos[0]);
+console.log(commits);
+    }
+    catch (err) {
+        console.log('Error', err.message);
+    }
+}
+displayCommits();
 
 console.log('After');
 
@@ -29,7 +43,8 @@ function getRepositories(username) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             console.log('Calling GitHub API...');
-            resolve (['Repo1', 'Repo2', 'Repo3']);
+            // resolve (['Repo1', 'Repo2', 'Repo3']);
+            reject(new Error('Could not get the repos.'));
         }, 2000);
     });
 }
