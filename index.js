3aa92d2fb9b80d5baab6c2a0a14ca84cb9855357
Chang Xiong <cnxiong03@gmail.com>
2019-12-08 22:39:37 -0500

    video 8 done

--- index.js
+++ index.js
@@ -1,29 +1,44 @@
 console.log('Before');
-getUser(1, getRepositories);
+// getUser(1, (user) => {
+//     getRepositories(user.gitHubUsername, (repos) => {
+//         getCommits(repos[0], (commits) => {
+//             console.log(commits);
+//             })
+//         });
+//     });
+
+getUser(1)
+    .then(user => getRepositories(user.gitHubUsername))
+    .then(repos => getCommits(repos[0]))
+    .then(commits => console.log('Commits', commits))
+    .catch(err => console.log('Error', err.message));
+
 console.log('After');
 
-function getRepositories(user) {
-    getRepositories(user.gitHubUsername, getCommits);
+function getUser(id) {
+    return new Promise((resolve, reject) => {
+        //Kickoff some async work
+        setTimeout(() => {
+            console.log('Reading a user from a database...');
+            resolve ({ id: id, gitHubUsername: 'Chang' });
+        }, 2000);
+    });
 }
 
-function getCommits(repos) {
-    getCommits(repo, displayCommits);
+function getRepositories(username) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Calling GitHub API...');
+            resolve (['Repo1', 'Repo2', 'Repo3']);
+        }, 2000);
+    });
 }
 
-function displayCommits(commits) {
-    console.log(commits);
-}
-
-function getUser(id, callback) {
-    setTimeout(() => {
-        console.log('Reading a user from a database...');
-        callback ({ id: id, gitHubUsername: 'Chang' });
-    }, 2000);
-};
-
-function getRepositories(username, callback) {
-    setTimeout(() => {
-        console.log('Calling GitHub API...');
-        callback (['Repo1', 'Repo2', 'Repo3']);
-    }, 2000);
+function getCommits(repo) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Calling GitHub API...');
+            resolve (['commit']);
+        },2000);
+    });
 }
